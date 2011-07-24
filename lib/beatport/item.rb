3a9441ba38a6dc0e38e49872057a50b14c8b58bb
Mateo Murphy <mateo.murphy@gmail.com>
2011-07-24 11:21:18 -0400

    Fixed class variable bug with Item associations

--- lib/beatport/item.rb
+++ lib/beatport/item.rb
@@ -1,15 +1,19 @@
 module Beatport
   
   class Item < OpenStruct
-    @@associations = {}
-        
+    class << self
+      def associations
+        @associations ||= {}
+      end
+    end
+  
     def initialize(data = {})
       raise ArgumentError, "Invalid data passed to Item.new: #{data.inspect}" unless data.is_a?(Hash)
             
       # OpenStruct doesn't like ids in its data, so we store it after the call to super
       id = data.delete('id')
       
-      @@associations.each do |k, v|
+      self.class.associations.each do |k, v|
         associate(data, k, v[:list], v[:klass])
       end
       
@@ -27,20 +31,20 @@ module Beatport
       send(key) if respond_to?(key)
     end
     
-    def associate(data, var, many = false, klass = Item)
+    def associate(data, var, collection = false, klass = Item)
       a = data.delete(var.to_s)
       
       return unless a
       
-      if many
+      if collection && a.is_a?(Array)
         a.map! { |g| klass.new(g) }
-      elsif a.is_a?(Hash)
+      elsif !collection && a.is_a?(Hash)
         a = klass.new(a)
       elsif a == []
         # In some cases, when there's no data returned for an association it'll be an empty array instead of a hash
         a = nil
       else
-        raise ArgumentError, "Invalid data for association: #{a.inspect}"
+        raise ArgumentError, "Invalid data for association: '#{var}' = #{a.inspect}"
       end
       
       instance_variable_set(:"@#{var}", a)
@@ -50,13 +54,13 @@ module Beatport
     def self.has_one(var, klass)
       self.send(:attr_reader, var)
 
-      @@associations[var] = {:list => false, :klass => klass}
+      self.associations[var] = {:list => false, :klass => klass}
     end
     
     def self.has_many(var, klass)
       self.send(:attr_reader, var)
 
-      @@associations[var] = {:list => true, :klass => klass}
+      self.associations[var] = {:list => true, :klass => klass}
     end
     
   end
