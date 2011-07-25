53b9f627348e0092dcefbf4c520d4fe15d4a48e5
Mateo Murphy <mateo.murphy@gmail.com>
2011-07-25 16:49:56 -0400

    Add camelize and underscore functions for future usage

--- lib/beatport/support.rb
+++ lib/beatport/support.rb
@@ -3,5 +3,23 @@ module Beatport
     def self.constantize(string)
       Beatport.const_get(string.capitalize)      
     end
+    
+    def self.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
+      if first_letter_in_uppercase
+        lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      else
+        lower_case_and_underscored_word.to_s[0].chr.downcase + camelize(lower_case_and_underscored_word)[1..-1]
+      end
+    end    
+    
+    def self.underscore(camel_cased_word)
+      word = camel_cased_word.to_s.dup
+      word.gsub!(/::/, '/')
+      word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+      word.tr!("-", "_")
+      word.downcase!
+      word
+    end
   end
 end
