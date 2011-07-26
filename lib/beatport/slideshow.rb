6a059232bc054c5e4098e01e1ce9aec17d47f928
Mateo Murphy <mateo.murphy@gmail.com>
2011-07-26 16:20:55 -0400

    Added finder for slideshow, as well as lazy loading of slideshow types

--- lib/beatport/slideshow.rb
+++ lib/beatport/slideshow.rb
@@ -1,6 +1,28 @@
 module Beatport
   class Slideshow < Item
+    attr_accessor :genre_id
+    
     has_many :header, Slide
+    has_many :feature, Slide
     has_many :small, Slide
+    
+    def self.find(id = nil)
+      obj = Slideshow.new
+      obj.genre_id = id
+      obj
+    end
+    
+    def header
+      @header ||= Slide.header(:genre_id => genre_id)
+    end
+
+    def feature
+      @feature ||= Slide.feature(:genre_id => genre_id)
+    end
+
+    def small
+      @small ||= Slide.small(:genre_id => genre_id)
+    end
+    
   end
 end
