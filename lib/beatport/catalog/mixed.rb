8e28ae21bfc31b793aaeb629fdd5aa256ff0e724
Mateo Murphy <mateo.murphy@gmail.com>
2011-12-18 15:00:37 -0500

    Implemented mixed search

---
+++ lib/beatport/catalog/mixed.rb
@@ -0,0 +1,9 @@
+module Beatport
+  module Catalog  
+    class Mixed
+      def self.all(options = {})
+        Client.retrieve('mixed', :auto, options)
+      end
+    end
+  end
+end
---
+++ spec/catalog/mixed_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+module Beatport::Catalog
+  describe Mixed do
+    describe '.all' do
+      before(:all) do
+        @results = Mixed.all(
+          :label_ids => [804,1390],
+          :artist_ids => [7181, 10395],
+          :chart_ids => [15722, 29514],
+          :release_ids => [164808, 385763],
+          :track_ids => [1873426,1746687]
+        )
+      end
+
+      it "should return two of each type" do
+        @results.map(&:type).should == ['label', 'label', 'artist', 'artist', 'chart', 'chart', 'release', 'release', 'track', 'track']
+      end
+
+      it "should return the right ids" do
+        @results.map(&:id).should == [804, 1390, 7181, 10395, 15722, 29514, 164808, 385763, 1746687, 1873426]
+      end
+
+    end
+  end
+end
