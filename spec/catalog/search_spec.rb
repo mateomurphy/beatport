cad656dbb7ec5bad1f09f780131c979e15ef9b54
Mateo Murphy <mateo.murphy@gmail.com>
2012-10-09 21:25:21 -0400

    Updated search spec and added vcr cassettes

--- spec/catalog/search_spec.rb
+++ spec/catalog/search_spec.rb
@@ -4,22 +4,34 @@ module Beatport::Catalog
   describe Search do
     describe '.get' do
       it "should perform a simple search with just a string" do
-        pending "Support for DJprofiles needs to be added"
-
-        results = Search.query('believe 2004')
-        results.length.should be >= 1
+        VCR.use_cassette("search_believe_2004") do
+          results = Search.query('believe 2004')
+          results.length.should be >= 1
+        end
       end
       
+      it "should perform a simple search with just a string" do
+        VCR.use_cassette("search_archipel") do
+          results = Search.query('Archipel')
+          results.length.should be >= 1
+        end
+      end      
+      
+
       it "should not find a release by catalogue number" do
-        results = Search.query("ANJCDCO011D")
-        results.length.should be 0
+        VCR.use_cassette("search_ANJCDCO011D") do        
+          results = Search.query("ANJCDCO011D")
+          results.length.should be 0
+        end
       end
       
       it "should find all content for Anjunadeep that has something to do with Trance" do
-        results = Search.query('anjunadeep', :facets => { :genre_name => 'Trance' })
-        results.length.should be > 1
-        
-        results.grouped.keys.sort.should == ["Chart", "Label", "Release", "Track"]
+        VCR.use_cassette("search_anjunadeep_genre_trance") do         
+          results = Search.query('anjunadeep', :facets => { :genre_name => 'Trance' })
+          results.length.should be > 1
+          
+          results.grouped.keys.sort.should == ["Chart", "Label", "Release", "Track"]
+        end
       end
     end
   end
