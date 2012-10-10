8ca4a6945202e4c5867cc17c534a6d171838daf3
Mateo Murphy <mateo.murphy@gmail.com>
2012-10-09 21:05:58 -0400

    Configure vcr and added some fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'beatport'
 require 'pp'
 require 'log_buddy'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures'
+  c.hook_into :fakeweb
+  c.allow_http_connections_when_no_cassette = true
+end
 
 RSpec.configure do |config|
   # some (optional) config here
